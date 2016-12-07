4d8383feaec0c6888d434ba06ddb5b237427b9de
yedohkang <kang.yedoh@gmail.com>
2016-12-06 20:16:43 -0500

    updated hw

--- hw40/Rational.java
+++ hw40/Rational.java
@@ -1,9 +1,24 @@
 // Yedoh Kang
 // APCS1 -- pd5
-// HW#37 -- Be More Rational
-// 2016-12-01
+// HW#40 -- Rational Equality
+// 2016-12-07
 
-public class Rational {
+/*============
+SUMMARY:
+Rational class performs many functions:
+- addition
+- subtraction
+- multiplication
+- division
+- floatValue
+- compareTo (>, < or =)
+- equals (same fraction?)
+- reduce
+- gcd
+of a given fraction.
+=============*/
+
+public class Rational implements Comparable{
     
 	// instance variables
 	private int p;
@@ -146,9 +161,13 @@ public class Rational {
 	}
 	
 	// compares Rational object taken in with the calling object  
-	public int compareTo (Rational rational) {
-		int numerator = rational.p;
-		int denominator = rational.q;
+	// implements Comparable by allowing the parameter to be of any object,
+	// but typecasts the given argument into an object that is of the same type
+	// as the class
+	public int compareTo (Object rational) {
+		Rational r = (Rational) rational; 
+		int numerator = r.p;
+		int denominator = r.q;
 		int number1 = p * denominator;
 		int number2 = q * numerator;
 		if (number1 == number2) {
@@ -161,6 +180,16 @@ public class Rational {
 			return -1;
 		}
 	}
+	
+	// takes in a Rational object
+	// returns true if and only if compareTo of both objects returns 0
+	// and if the given argument is an instance of the class Rational
+	public boolean equals (Rational rational) {
+		if (rational instanceof Rational && compareTo(rational) == 0) {
+			return true;
+		}
+		return false;
+	}
 
 	public static void main (String [] args) {
 		Rational r = new Rational(2,3); // Stores the rational number 2/3
@@ -168,44 +197,66 @@ public class Rational {
 		Rational t = new Rational(4,18); // Stores the rational number 4/18
 		Rational u = new Rational(2,9); // Stores the rational number 4/18
 
+		// float value
 		System.out.println("R's floatValue = " + r.floatValue());
 		System.out.println("S's floatValue = " + s.floatValue());
 		System.out.println("T's floatValue = " + t.floatValue() + "\n");
 
+		// addition
 		System.out.println("Addition");
 		r.add(s); // Adds r to s, changes r to 7/6. s remains 1/2
 		System.out.println("R is: \n" + r);
 		System.out.println("S is: \n" + s + "\n");
 
+		// substraction
 		System.out.println("Subtraction");
 		r.subtract(s); // Subtracts s from r, changes r to 1/6. s remains 1/2
 		System.out.println("R is: \n" + r);
 		System.out.println("S is: \n" + s + "\n");
     
+		// multiplication
 		System.out.println("Multiplication");
 		r.multiply(s); // Multiplies r by s, changes r to 2/6.  s remains 1/2
 		System.out.println("R is: \n" + r);
 		System.out.println("S is: \n" + s + "\n");
     
+		// division
 		System.out.println("Division");
 		r.divide(s); // Divides r by s, changes r to 2/1.5.  s remains 1/2
 		System.out.println("R is: \n" + r);
 		System.out.println("S is: \n" + s + "\n");
 		
+		// reduce
 		System.out.println("Reduce");
 		t.reduce(); // reduces 4/18 to 2/9
 		System.out.println("T is: \n" + t + "\n");
 		
+		// gcd
 		System.out.println("GCD");
 		int n = gcd(4,18);
 		System.out.println("GCD of 4/18 is: " + n + "\n");
 		
+		// compareTo
 		System.out.println("CompareTo");
 		// should be 1
 		System.out.println("R compared with S is: " + r.compareTo(s));
 		// should be -1
 		System.out.println("S compared with R is: " + s.compareTo(r));
 		// should be 0
-		System.out.println("T compared with U is: " + t.compareTo(u));
+		System.out.println("T compared with U is: " + t.compareTo(u) + "\n");
+		
+		Rational z = new Rational(2,3); // Stores the rational number 2/3
+		Rational w = new Rational(2,3); // Stores the rational number 2/3
+		Rational y = new Rational(4,6); // Stores the rational number 1/2
+		Rational x = new Rational(4,18); // Stores the rational number 4/18
+		
+		// .equals()
+		System.out.println("Equals");
+		// should be true
+		System.out.println("Z.equals(W) is: " + z.equals(w));
+		// should be true
+		System.out.println("Z.equals(Y) is: " + z.equals(y));
+		// should be false
+		System.out.println("Z.equals(X) is: " + z.equals(x));
 	}
 }
