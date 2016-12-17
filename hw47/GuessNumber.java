6c354e40d3cb8c9c7afb293d024af859fb4cf2c8
yedohkang <kang.yedoh@gmail.com>
2016-12-17 11:20:41 -0500

    hw47

---
+++ hw47/GuessNumber.java
@@ -0,0 +1,68 @@
+// Yedoh Kang
+// APCS1 -- pd5
+// HW#47 -- Keep Guessing
+// 2016-12-19
+
+import cs1.Keyboard;
+
+public class GuessNumber {
+	
+	private static int numGuess; // number of guesses
+	private static int number; // chosen number by program
+	private static int guess; // guess by user
+	private static int lowerBound = 1; // lower range
+	private static int upperBound = 100; // upper range
+	
+	public static void choose () {
+		number = (int) (Math.random() * 100);
+	}
+	
+	public static void main (String[] args) {
+		
+		choose();
+		
+		System.out.println("Hi! Let's play the guessing game!\n");
+		
+		System.out.print("Guess a number from 1-100: ");
+		
+		guess = Keyboard.readInt();
+		numGuess += 1;
+		
+		if (guess == number) {
+			
+			System.out.println("Correct! It took 1 guess.");
+			
+		} 
+		
+		else {
+			
+			while (guess != number) {
+				
+				if (guess < number) {
+					
+					lowerBound = guess + 1;
+					
+					System.out.println("Too low, try again...\n");
+					System.out.print("Guess a number from " + lowerBound + "-" + upperBound + ": ");
+		
+					guess = Keyboard.readInt();
+					numGuess += 1;
+				}
+				
+				else {
+				
+					upperBound = guess - 1;
+					
+					System.out.println("Too high, try again...\n");
+					System.out.print("Guess a number from " + lowerBound + "-" + upperBound + ": ");
+		
+					guess = Keyboard.readInt();
+					numGuess += 1;
+				}
+			}
+			
+			System.out.println("Correct! It took " + numGuess + " guesses.");
+		}	
+	}
+	
+}
